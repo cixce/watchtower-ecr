df30132698d79a38d1ae5fc209a3f63ab2fc1ac8
Cian Montgomery <cian@cianmontgomery.com>
2022-01-01 13:02:20 -0800

    Update to change package naming

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,10 +3,9 @@
 set -x
 DOCKER_TOKEN=$1
 
-DOCKER_IMAGE_NAME=watchtower
 DOCKER_IMAGE_TAG=latest
 
-DOCKER_IMAGE_NAME=$(echo ghcr.io/${GITHUB_REPOSITORY}/${DOCKER_IMAGE_NAME} | tr '[:upper:]' '[:lower:]')
+DOCKER_IMAGE_NAME=$(echo ghcr.io/${GITHUB_REPOSITORY} | tr '[:upper:]' '[:lower:]')
 DOCKER_IMAGE_NAME_WITH_TAG=$(echo ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} | tr '[:upper:]' '[:lower:]')
 
 docker login -u publisher -p ${DOCKER_TOKEN} ghcr.io
